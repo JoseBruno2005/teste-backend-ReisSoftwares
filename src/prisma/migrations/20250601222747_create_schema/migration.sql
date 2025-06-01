c56c9668d992a3ce1881e241777990a087395170
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2025-06-01 19:29:22 -0300

    refactor(prisma): renomeia campo mail para email no model User

--- src/prisma/migrations/20250601222747_create_schema/migration.sql
+++ src/prisma/migrations/20250601222747_create_schema/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "mail" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -24,7 +24,7 @@ CREATE TABLE "Task" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_mail_key" ON "User"("mail");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
